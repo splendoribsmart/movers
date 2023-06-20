a2c25148750fc433e6fbbe3b89207134bbf8591c
splendoribsmart <splendoribsmart@gmail.com>
2023-06-20 09:09:41 +0100

    first

---
+++ page/views.py
@@ -0,0 +1,146 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context = {}
+    return render(request, "www.hireahelper.com/index.html", context)
+
+def about(request):
+    context = {}
+    return render(request, "www.hireahelper.com/about/index.html", context)
+
+def affiliate(request):
+    context = {}
+    return render(request, "www.hireahelper.com/affiliate/index.html", context)
+
+def contact(request):
+    context = {}
+    return render(request, "www.hireahelper.com/contact/index.html", context)
+
+def covid(request):
+    context = {}
+    return render(request, "www.hireahelper.com/covid/index.html", context)
+
+def day_labor(request):
+    context = {}
+    return render(request, "www.hireahelper.com/day-labor/index.html", context)
+
+def claims(request):
+    context = {}
+    return render(request, "www.hireahelper.com/help/claims/index.html", context)
+
+def faq(request):
+    context = {}
+    return render(request, "www.hireahelper.com/help/faq/index.html", context)
+
+def ca_privacy(request):
+    context = {}
+    return render(request, "www.hireahelper.com/legal/california-privacy-notice/index.html", context)
+
+def guarantee(request):
+    context = {}
+    return render(request, "www.hireahelper.com/legal/guarantee/index.html", context)
+
+def license(request):
+    context = {}
+    return render(request, "www.hireahelper.com/legal/license-and-insurance/index.html", context)
+
+def privacy(request):
+    context = {}
+    return render(request, "www.hireahelper.com/legal/privacy/index.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "www.hireahelper.com/login/index.html", context)
+
+def legal(request):
+    context = {}
+    return render(request, "www.hireahelper.com/legal/index.html", context)
+
+def furniture(request):
+    context = {}
+    return render(request, "www.hireahelper.com/move-help/furniture-movers-assembly/index.html", context)
+
+def loading(request):
+    context = {}
+    return render(request, "www.hireahelper.com/move-help/loading/index.html", context)
+
+def packing(request):
+    context = {}
+    return render(request, "www.hireahelper.com/move-help/packing-help/index.html", context)
+
+def unloading(request):
+    context = {}
+    return render(request, "www.hireahelper.com/move-help/unloading-help/index.html", context)
+
+def move_help(request):
+    context = {}
+    return render(request, "www.hireahelper.com/move-help/index.html", context)
+
+def movers_academy(request):
+    context = {}
+    return render(request, "www.hireahelper.com/movers-academy/index.html", context)
+
+def movers_near_me(request):
+    context = {}
+    return render(request, "www.hireahelper.com/movers-near-me/index.html", context)
+
+def moving(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving/index.html", context)
+
+def corporate_relocation(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving/corporate-relocation/index.html", context)
+
+def military_move(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving/military-move/index.html", context)
+
+def moving_containers(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving/moving-containers/index.html", context)
+
+def office_movers(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving/office-movers/index.html", context)
+
+def rental_truck(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving/rental-truck/index.html", context)
+
+def semi_freight_trailer(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving/semi-freight-trailer/index.html", context)
+
+def moving_companies(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving-companies/index.html", context)
+
+def long_distance(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving-companies/long-distance/index.html", context)
+
+def moving_cost(request):
+    context = {}
+    return render(request, "www.hireahelper.com/moving-cost/index.html", context)
+
+def my_move(request):
+    context = {}
+    return render(request, "www.hireahelper.com/my-move/index.html", context)
+
+def how_it_works(request):
+    context = {}
+    return render(request, "www.hireahelper.com/my-move/how-it-works/index.html", context)
+
+def moving_cost(request):
+    context = {}
+    return render(request, "www.hireahelper.com/my-move/moving-day/index.html", context)
+
+def sign_up(request):
+    context = {}
+    return render(request, "www.hireahelper.com/sign-up/index.html", context)
+
+def sitemap(request):
+    context = {}
+    return render(request, "www.hireahelper.com/sitemap/index.html", context)
